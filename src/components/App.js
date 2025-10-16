b0f20a368746c6e3b9ed549b72fffbfd4dd666fc
Pranjal <pranjal7903@email.com>
2025-10-16 16:13:22 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,17 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import Review from './Review'; // Adjust the path as needed based on your file structure
 
+const App = () => {
+  return (
+    <div>
+      <section>
+        <h1>Our Reviews</h1>
+        <main>
+          <Review />
+        </main>
+      </section>
+    </div>
+  );
+};
+
+export default App;
